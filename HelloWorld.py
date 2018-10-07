0df986b708976e1daf11ab9dfd8113e34a76de5a
manikchaudhery <28273430+manikchaudhery@users.noreply.github.com>
2018-10-07 19:03:24 -0400

    upldated main helloworld file

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,60 +1,53 @@
-from bottle import route, run, template, Response, request, view
-
+from bottle import route, run, template, Response, request
 topOccurences = dict()
-
-
 @route('/')
 def index():
-    return template('index')
-
+	return template('index')
 
 @route('/', method='POST')
-#@view('object.tpl')
 def doCounting():
     searchSentence = request.forms.get('search')
-
-    occurences = countNumberOfWords(searchSentence)
-    print (occurences)
-    return template('object', occurences=occurences)
-
+    print(countNumberOfWords(searchSentence))
+    
 
 def countNumberOfWords(sentence):
-    wordsArray = sentence.split()
-    occurences = dict()
+	wordsArray = sentence.split()
+	occurences = dict()
+	
 
-    for word in wordsArray:
-        if word not in occurences:
-            occurences[word] = 1
-        else:
-            occurences[word] += 1
+	for word in wordsArray:
+		if word not in occurences:
+			occurences[word] = 1
+		else: 
+			occurences[word] += 1
 
-        if word not in topOccurences:
-            topOccurences[word] = 1
-        else:
-            topOccurences[word] += 1
-
-    result = sorted(topOccurences.items(), key=lambda t: t[1], reverse=True)
-    print("printing result now")
-    for k, v in result:
-        print(k, v)
-    print("occurences in funcion are ", occurences)
-    return occurences
+		if word not in topOccurences:
+			topOccurences[word] = 1
+		else: 
+			topOccurences[word] += 1	
+	
+	result = sorted(topOccurences.items() , key=lambda t : t[1] , reverse=True)
+	print("printing result now")
+	for k,v in result:
+		print(k,v)
+	return template('object', occurences = occurences)
+	
 
 
 @route('/querytest')
 def querytest():
-    param1 = request.query.param1
-    param2 = request.query.param2
-
+	param1 = request.query.param1
+	param2 = request.query.param2
 
 if __name__ == '__main__':
-    run(host='localhost', port=8080, debug=False, reloader=True)
+
+	run(host='localhost', port=3006, debug=False, reloader = True)
 
 '''
 <!DOCTYPE=HTML>
         <form action="/login" method="post">
            <input name="Search" type="text" />
-
+            
             <input value="Search" type="submit" />
         </form>
 
