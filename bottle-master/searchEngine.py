da6da80e188907512ebdfbe598f4fda977148079
manikchaudhery <28273430+manikchaudhery@users.noreply.github.com>
2018-11-12 22:18:28 -0500

    Update searchEngine.py

--- bottle-master/searchEngine.py
+++ bottle-master/searchEngine.py
@@ -1,6 +1,6 @@
 # importing the required modules
 import bottle
-from bottle import route, run, template, response, request, view, static_file, get, post, app
+from bottle import route, run, template, response, error, request, view, static_file, get, post, app
 from beaker.middleware import SessionMiddleware
 from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.client import flow_from_clientsecrets
@@ -8,10 +8,24 @@ from googleapiclient.discovery import build
 import json
 import httplib2
 import requests
+from paginator import finder
+
+
 
 # the following is the dictionary that keeps a record of the top twenty words
 topOccurences = dict()
 userHistory = dict()
+topOccurences = dict()
+userHistory = dict()
+docsSorted = list()
+results_per_page = 5
+page = 1
+firstWord = str()
+pagesNeeded = 0
+upperCount = 0
+lowerCount = 0
+listOfLists = list()
+currentPage = 1
 
 session_opts = {
     'session.auto': True,
@@ -35,7 +49,7 @@ def getMethod():
     if userSignedIn:
         counter += 1
         print(counter)
-        bottle.redirect("http://localhost:8080/login")
+        bottle.redirect("http://0.0.0.0:80/login")
 
     # if user is already logged in
     return template("object")
@@ -44,13 +58,16 @@ def getMethod():
 @post('/')
 def index():
     global userSignedIn
-
+    global docsSorted
+    global firstWord
+    global pagesNeeded
     occurencesList = []
     sortedTopTwentyDictionary = dict()
     searchSentence = ""
 
     # getting the sentence entered by the user
     searchSentence = request.forms.get('search')
+    print('search sentence is: ', searchSentence)
 
     # making sure not to pass in an empty string
     if (searchSentence != None):
@@ -60,12 +77,110 @@ def index():
         # converting to list to arrange in descending order by value
         occurencesList = sorted(occurences.items(), key=lambda t: t[1], reverse=True)
         print(occurencesList)
-
+    num = 1
     # passing essential details to the template to dsiplay on the front page
     picture_name = "logo_transparent.png"
+    if (searchSentence != None):
+        firstWord = searchSentence.lower().split()[0]
+        docsSorted = finder(firstWord)
+        if (docsSorted == 0):
+            bottle.redirect("http://0.0.0.0:80/urlNonExistent")
+        if len(docsSorted) <= 5:
+            return template('index', occurences=occurencesList,
+                            picture=picture_name, searchSentence=searchSentence, urlsList=docsSorted)
+        else:
+            remainder = len(docsSorted) % 5
+            print('remainder is: ', remainder)
+            counter = 0
+            if remainder == 0:
+                pagesNeeded = len(docsSorted) / 5
+                print('yahan hun')
+                print(pagesNeeded)
+            else:
+                print('ithe')
+                pagesNeeded = (len(docsSorted) // 5) + 1
+                print(pagesNeeded)
+            newURl = "http://0.0.0.0:80/results/1"
+            bottle.redirect(newURl)
 
-    return template('index', occurences=occurencesList,
-                    picture=picture_name, searchSentence=searchSentence)
+    if(docsSorted == 0):
+        bottle.redirect("http://0.0.0.0:80/urlNonExistent")
+
+    return template('index',
+                    picture=picture_name, urlsList=docsSorted)
+
+@get('/results/<pageNumber>')
+def displayResults(pageNumber):
+    global docsSorted
+    global page
+    global results_per_page
+    global firstWord
+    global userSignedIn
+
+    global pagesNeeded
+    global upperCount
+    global lowerCount
+    global currentPage
+
+    currentPage = int(pageNumber)
+    print("earlier current page number is: ", currentPage)
+
+    if(docsSorted == 0):
+        bottle.redirect("http://0.0.0.0:80/urlNonExistent")
+    nextPage = 0
+    previousPage = 0
+    upperCount = 5
+    lowerCount = 0
+    newdocs = list()
+    reip = 1
+    while reip <= pagesNeeded:
+
+        newList = docsSorted[lowerCount:upperCount]
+        #print('printing new list: ')
+        #print(newList)
+        listOfLists.append(newList)
+        lowerCount = upperCount
+        upperCount += 5
+        reip += 1
+    #print('list of lists')
+    #print(listOfLists)
+    #print(lowerCount)
+    #print(upperCount)
+    newdocs = listOfLists[currentPage-1]
+
+    if currentPage == pagesNeeded:
+        nextPage = pagesNeeded
+        picture_name = "logo_transparent.png"
+        #print('pages needed is: ', pagesNeeded)
+        previousPage = currentPage - 1
+        return template('noNextButton', previousPage=previousPage,
+                        picture=picture_name, urlsList=newdocs, currentPage=currentPage, listOfLists=listOfLists,
+                        pagesNeeded=pagesNeeded)
+
+
+
+
+
+    if currentPage == 1:
+        previousPage = 1
+        nextPage = currentPage + 1
+        picture_name = "logo_transparent.png"
+        #print('pages needed is: ', pagesNeeded)
+        return template('noPreviousButton', nextPage=nextPage,
+                        picture=picture_name, urlsList=newdocs, currentPage=currentPage, listOfLists=listOfLists,
+                        pagesNeeded=pagesNeeded)
+
+    nextPage = currentPage + 1
+
+    previousPage = currentPage - 1
+
+
+    picture_name = "logo_transparent.png"
+    #print('pages needed is: ', pagesNeeded)
+    return template('newIndex', nextPage=nextPage, previousPage=previousPage,
+                    picture=picture_name, urlsList=newdocs, currentPage=currentPage, listOfLists=listOfLists, pagesNeeded=pagesNeeded)
+
+
 
 
 #following oauth google documentation
@@ -75,11 +190,15 @@ def login():
     userSignedIn = True
 
     flow = flow_from_clientsecrets("client_secret.json",
-                                   redirect_uri="http://localhost:8080/redirect",
+                                   redirect_uri="http://0.0.0.0:80/redirect",
                                    scope='https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email'
                                    )
     uri = flow.step1_get_authorize_url()
     bottle.redirect(str(uri))
+
+@get('/urlNonExistent')
+def urlNonExistent():
+    return template('error')
 
 #user gets here when logged in
 @get('/redirect')
@@ -87,7 +206,7 @@ def redirect_page():
     global userSignedIn
 
     if not userSignedIn:
-        bottle.redirect("http://localhost:8080")
+        bottle.redirect("http://0.0.0.0:80")
     code = request.query.get('code', '')
 
 
@@ -103,7 +222,7 @@ def redirect_page():
     scope = 'https://www.googleapis.com/auth/plus.me https://www.googleapis.com/auth/userinfo.email'
 
     flow = OAuth2WebServerFlow(client_id=client_id, client_secret=client_secret, scope=scope,
-                               redirect_uri="http://localhost:8080/redirect")
+                               redirect_uri="http://0.0.0.0:80/redirect")
     credentials = flow.step2_exchange(code)
     token = credentials.id_token['sub']
 
@@ -130,6 +249,9 @@ def redirect_page():
 def displayResults():
     user_email = request.get_cookie("email")
     global userHistory
+    global docsSorted
+    global firstWord
+    global pagesNeeded
 
     # results table
     # getting the sentence entered by the user
@@ -162,12 +284,108 @@ def displayResults():
         topTwenty = sorted(newOccurences.items(), key=lambda t: t[1], reverse=True)
         topTwenty = topTwenty[:20]
 
-#passing essential details to the template to dsiplay on the front page
+#passing essential details to the template to display on the front page
     picture_name = "logo_transparent.png"
-    return template('loggedInResults', occurences=occurencesList, sortedTopTwentyDictionary=topTwenty,
-					picture=picture_name, searchSentence=searchSentence, user_email=user_email)
+    if (searchSentence != None):
+        firstWord = searchSentence.lower().split()[0]
+        docsSorted = finder(firstWord)
+        if (docsSorted == 0):
+            bottle.redirect("http://0.0.0.0:80/urlNonExistent")
+        if len(docsSorted) <= 5:
+            return template('loggedInResults',
+                            picture=picture_name, searchSentence=searchSentence, urlsList=docsSorted, user_email=user_email)
+        else:
+            remainder = len(docsSorted) % 5
+            print('remainder is: ', remainder)
+            counter = 0
+            if remainder == 0:
+                pagesNeeded = len(docsSorted) / 5
+                print('yahan hun')
+                print(pagesNeeded)
+            else:
+                print('ithe')
+                pagesNeeded = (len(docsSorted) // 5) + 1
+                print(pagesNeeded)
+            newURl = "http://0.0.0.0:80/resultsLoggedIn/1"
+            bottle.redirect(newURl)
+
+    if(docsSorted == 0):
+        bottle.redirect("http://0.0.0.0:80/urlNonExistent")
 
 
+@get('/resultsLoggedIn/<pageNumber>')
+def displayResults(pageNumber):
+    user_email = request.get_cookie("email")
+    global docsSorted
+    global page
+    global results_per_page
+    global firstWord
+    global userSignedIn
+
+    global pagesNeeded
+    global upperCount
+    global lowerCount
+    global currentPage
+
+    currentPage = int(pageNumber)
+
+    nextPage = 0
+    previousPage = 0
+    upperCount = 5
+    lowerCount = 0
+    newdocs = list()
+    reip = 1
+    while reip <= pagesNeeded:
+
+        newList = docsSorted[lowerCount:upperCount]
+        print('printing new list: ')
+        print(newList)
+        listOfLists.append(newList)
+        lowerCount = upperCount
+        upperCount += 5
+        reip += 1
+    print('list of lists')
+    print(listOfLists)
+    print(lowerCount)
+    print(upperCount)
+    newdocs = listOfLists[currentPage-1]
+
+    if currentPage == pagesNeeded:
+        nextPage = pagesNeeded
+        previousPage = currentPage - 1
+        picture_name = "logo_transparent.png"
+        print('pages needed is: ', pagesNeeded)
+        return template('noNextButtonLoggedIn',nextPage=nextPage, previousPage=previousPage,
+                    picture=picture_name, urlsList=newdocs, currentPage=currentPage, listOfLists=listOfLists, pagesNeeded=pagesNeeded, user_email=user_email)
+
+
+
+
+
+    if currentPage == 1:
+        previousPage = 1
+        nextPage = currentPage + 1
+        picture_name = "logo_transparent.png"
+        print('pages needed is: ', pagesNeeded)
+        return template('noPreviousButtonLoggedIn', nextPage=nextPage, previousPage=previousPage,
+                    picture=picture_name, urlsList=newdocs, currentPage=currentPage, listOfLists=listOfLists, pagesNeeded=pagesNeeded, user_email=user_email)
+
+
+    nextPage = currentPage + 1
+
+    previousPage = currentPage - 1
+
+    picture_name = "logo_transparent.png"
+    print('pages needed is: ', pagesNeeded)
+    return template('newLoggedInResults', nextPage=nextPage, previousPage=previousPage,
+                    picture=picture_name, urlsList=newdocs, currentPage=currentPage, listOfLists=listOfLists, pagesNeeded=pagesNeeded, user_email=user_email)
+
+
+
+
+@error(404)
+def error404(error):
+    return template('error')
 
 #defines the logout method
 @get('/logout')
@@ -180,7 +398,7 @@ def logout():
     global userSignedIn
     userSignedIn = False
 
-    bottle.redirect("http://0.0.0.0:8080")
+    bottle.redirect("http://0.0.0.0:80")
 
 # enables search engine logo to be displayed
 @route('/static/<filename>')
